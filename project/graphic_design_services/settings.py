9807d0802b409d599128a5a34131a610a42bf2ea
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2019-11-12 16:28:47 +0000

    settings file updated"

--- project/graphic_design_services/settings.py
+++ project/graphic_design_services/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_LOCATION = "static"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
