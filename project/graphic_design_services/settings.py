55c3937c378aa2ddca3d7b867cf00d8eba9d03ee
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2019-11-12 16:20:37 +0000

    settings file updated

--- project/graphic_design_services/settings.py
+++ project/graphic_design_services/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = [os.environ.get('SECRET_KEY')]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = [os.environ.get('C9_PREVIEW'), 'my-graphic-design-services.herokuapp.com/']
+ALLOWED_HOSTS = [os.environ.get('C9_PREVIEW'), os.environ.get('HEROKU_DOMAIN')]
 
 # Application definition
 
