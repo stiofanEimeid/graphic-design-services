35ab1287ccc26e54695743a694b4230e25eae198
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2019-12-13 19:15:24 +0000

    requirements.txt and settings file updated

--- project/graphic_design_services/settings.py
+++ project/graphic_design_services/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
+    # 'django_forms_bootstrap',
     
     # own
     'pages.apps.PagesConfig',
