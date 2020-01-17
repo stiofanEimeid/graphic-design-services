55282a0caaafd9abd0b6951bb0fa18aae2868f3a
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-17 19:11:00 +0000

    password reset functionality added

--- project/graphic_design_services/settings.py
+++ project/graphic_design_services/settings.py
@@ -178,3 +178,10 @@ LOGIN_REDIRECT_URL = 'homepage'
 LOGIN_URL = 'login'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST =' smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =  os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_PASS')
