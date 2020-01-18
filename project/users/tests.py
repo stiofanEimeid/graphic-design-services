f614652db5fe252fe2c0b51772f1e59f90b348d3
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-18 18:50:34 +0000

    tests added to users module

--- project/users/tests.py
+++ project/users/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.shortcuts import reverse
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your tests here.
+
+
+# Forms
+
+class TestUserRegistrationForm(TestCase):
+    def test_registration_valid(self):
+            form = UserRegisterForm({'username':"testuser",
+                                         "email": "test@example.com",
+                                         "password1": "test_password",
+                                         "password2": "test_password",
+                                         })
+            self.assertTrue(form.is_valid())
+            
+    def test_passwords_do_not_match(self):
+        form = UserRegisterForm({"username": "TestUser",
+                                "email": "Test@Email.com",
+                                "password1": "tetspassword",
+                                "password2": "testpassword"})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["password2"], [u"Passwords do not match. Please enter matching passwords."])
+            
+    
+# class TestUserUpdateForm(TestCase):
+
+# class TestProfileUpdateForm(TestCase):
+
+# Views
+
+# profile
+
+# login
+
+# logout
+
+# register
