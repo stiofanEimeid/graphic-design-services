7615278d48d9e031086d64dd613931a6cfc4718f
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-22 15:13:24 +0000

    media folder added

--- project/graphic_design_services/settings.py
+++ project/graphic_design_services/settings.py
@@ -147,6 +147,10 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
