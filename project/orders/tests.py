8d4784805660ba0135b4d0a42b8ed2915ce09550
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-26 21:33:01 +0000

    new tests added to orders app

--- project/orders/tests.py
+++ project/orders/tests.py
@@ -2,7 +2,9 @@ from django.test import TestCase, Client
 from django.core.files.uploadedfile import SimpleUploadedFile
 from .forms import OrderForm, RevisionsForm, TestimonialForm, DesignAcceptanceForm, DesignSubmissionForm, DesignUpdateForm
 from users.models import User
-from .models import Design, Order
+from django.shortcuts import reverse, redirect
+from .models import Design, Order, Revision
+from .views import submit_design
 
 # Create your tests here.
 
@@ -58,14 +60,6 @@ class TestTestimonialForm(TestCase):
         self.assertEqual(
             form.errors["testimonial_text"], [u"This field is required."])
 
-# class TestDesignSubmissionForm(TestCase):
-
-## File Upload
-
-# class TestDesignUpdateForm(TestCase):
-
-## File Upload
-
 # class TestDesignAcceptanceForm(TestCase):
 
 class TestDesignAcceptanceForm(TestCase):
@@ -79,63 +73,125 @@ class TestDesignAcceptanceForm(TestCase):
 
 class TestOrdersView(TestCase):
     
-    def set_up(self):
-        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
-        test_user1.save()
-        test_admin = User.objects.create_superuser(username="testadmin", email="admin@example.com", password="784fhhv*90")
+    def setUp(self):
+        test_admin = User.objects.create_superuser(username='testadmin', email="admin@example.com", password='784fhhv*90')
         test_admin.save()
-        test_order = Order.objects.create(
-            customer = "testuser1",
-            type = "Logo",
-            description = 'This is a new feature description',
-            price = "10"
-            )
-        test_design = Design.objects.create(
-            type='Logo', 
-            description='This is a new feature description',
-            customer= "testuser1",
-            order_stage = "Design pending approval",
-            source_code= SimpleUploadedFile("test_source.jpg", b"file_content", content_type="image/jpeg"),
-            preview_image= SimpleUploadedFile("test__preview_image.jpg", b"file_content", content_type="image/jpeg"),
-            order_number = Order.objects.get(customer='testuser1')
-           )
+        self.user = test_admin
+        test_user2 = User.objects.create_user(username='testuser2', password='kjsnlkblmbl')
+        test_user2.save()
+        self.user = test_user2
+        
+    # def test_order_create_view(self):
+    #     login = self.client.login(username='testuser2', password='kjsnlkblmbl')
+    #     session = self.client.session
+    #     session['basket'] = {}
+    #     session['basket']['type'] = "Logo"
+    #     session['my_basket']['description'] = "Some text here"
+    #     session['my_basket']['revision'] = False
+    #     session['my_basket']['price'] = 20
+    #     session.save()
         
     def test_orders_view(self):
         page = self.client.get("/orders/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "orders.html")
         
-# gallery design detail
-    # def test_gallery_view(self):
-    #     design = Design.objects.get(customer = "testuser1")
-    #     page = self.client.get("gallery-design-detail/{}".format(design.pk))
-    #     self.assertEqual(page.status_code, 302)
-    
-
-
-### needs parameter
-
-
-## order list
-    # def order_list_view(self):
-    #     login = self.client.login(username='testadmin', password='784fhhv*90')
-    #     page = self.client.get("/order-list/")
-    #     self.assertEqual(page.status_code, 200)
-
+    def order_list_view(self):
+        login = self.client.login(username='testadmin', password='784fhhv*90')
+        page = self.client.get("/order-list/")
+        self.assertEqual(page.status_code, 200)
+        
+    def test_submit_design_view(self):
+        login = self.client.login(username='testadmin', password='784fhhv*90')
+        user = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        order = Order(
+            customer = user,
+            type = "Logo",
+            description = 'This is a new feature description',
+            price = "10")
+        order.save()
+        response = self.client.get(reverse('submit-design', kwargs={'parameter':order.id}))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_valid_design_submission(self):
+        login = self.client.login(username='testadmin', password='784fhhv*90')
+        user = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        order = Order(
+            customer = user,
+            type = "Logo",
+            description = 'This is a new feature description',
+            price = "10")
+        order.save()
+        response = self.client.post(reverse('submit-design', kwargs={'parameter':order.id,}), {
+            'source_code': SimpleUploadedFile("test_source.jpg", b"file_content", content_type="image/jpeg"),
+            'preview_image': SimpleUploadedFile("test_preview.jpg", b"file_content", content_type="image/jpeg"),
+        })
+        self.assertEqual(response.status_code, 200)
+        
 ## request changes
 
-### needs parameter
 
-## revision detail
+## test submit revision view
 
-### needs parameter
-
-## submit design
-        
-### needs parameter
+    def test_submit_revision_view(self):
+            login = self.client.login(username='testadmin', password='784fhhv*90')
+            user = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+            order = Order(
+                customer = user,
+                type = "Logo",
+                description = 'This is a new feature description',
+                price = "10")
+            order.save()
+            design = Design(
+                    customer = user,
+                    type = "Logo",
+                    description = 'This is a new description',
+                    order_number = order
+                    )
+            design.save()
+            revision = Revision(
+                customer = user,
+                type = "Logo",
+                revisions = 'This is a new feature description',
+                price = "10",
+                design_id = design
+                )
+            revision.save()
+            response = self.client.get(reverse('submit-revision', kwargs={'parameter':design.id}))
+            self.assertEqual(response.status_code, 200)
 
 ## submit revision
-
-### needs parameter
+    def test_valid_revision_submission(self):
+            login = self.client.login(username='testadmin', password='784fhhv*90')
+            user = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+            order = Order(
+                customer = user,
+                type = "Logo",
+                description = 'This is a new feature description',
+                price = "10")
+            order.save()
+            design = Design(
+                    customer = user,
+                    type = "Logo",
+                    description = 'This is a new description',
+                    order_number = order
+                    )
+            design.save()
+            revision = Revision(
+                customer = user,
+                type = "Logo",
+                revisions = 'This is a new feature description',
+                price = "10",
+                design_id = design
+                )
+            revision.save()
+            response = self.client.post(reverse('submit-revision', kwargs={'parameter':design.id,}), {
+                'source_code': SimpleUploadedFile("test_source.jpg", b"file_content", content_type="image/jpeg"),
+                'preview_image': SimpleUploadedFile("test_preview.jpg", b"file_content", content_type="image/jpeg"),
+            })
+            self.assertEqual(response.status_code, 200)
 
 ## submit testimonial
+
+# gallery design detail
+ 
