24f6ba6e73f6445973995f0397002e944c4e4e1f
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-31 12:25:12 +0000

    order detail and design detail templates updated with new model keys

--- project/users/views.py
+++ project/users/views.py
@@ -13,7 +13,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('homepage')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
