09ec05a47b06af5baca5ce41902a78351ae7b16b
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-31 09:27:12 +0000

    basket view/template updated. Css tidied

--- project/basket/views.py
+++ project/basket/views.py
@@ -4,12 +4,7 @@ from orders.forms import OrderForm
 
 def view_basket(request):
     """A view that renders the content of the basket"""
-    context = {
-            'type': request.session['my_basket']['type'],
-            'description': request.session['my_basket']['description'],
-            'price': request.session['my_basket']['price']
-        }
-    return render(request, "basket.html", context)
+    return render(request, "basket.html", {})
 
     
 
