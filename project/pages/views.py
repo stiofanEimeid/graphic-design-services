93d737707a8982971922153968791b3db48a921d
Ubuntu <ubuntu@ip-172-31-95-209.ec2.internal>
2020-01-31 12:04:05 +0000

    dead code removed

--- project/pages/views.py
+++ project/pages/views.py
@@ -35,19 +35,19 @@ def orders(request, *args, **kwargs):
 
 """Custom Handlers"""
 
-def handler403(request, exception, template_name="403.html"):
-    response = render_to_response("403.html")
-    response.status_code = 403
-    return response
+# def handler403(request, exception, template_name="403.html"):
+#     response = render_to_response("403.html")
+#     response.status_code = 403
+#     return response
 
 
-def handler404(request, exception, template_name="404.html"):
-    response = render_to_response("404.html")
-    response.status_code = 404
-    return response
+# def handler404(request, exception, template_name="404.html"):
+#     response = render_to_response("404.html")
+#     response.status_code = 404
+#     return response
 
 
-def handler500(request, exception, template_name="500.html"):
-    response = render_to_response("500.html")
-    response.status_code = 500
-    return response
+# def handler500(request, exception, template_name="500.html"):
+#     response = render_to_response("500.html")
+#     response.status_code = 500
+#     return response
